d7f75b31f867ef3db51862aa1a981923315e774f
Ramesh Appat <rameshappat@gmail.com>
2025-12-25 17:23:42 -0700

    feat: add SmsService.java

---
+++ src/main/java/com/northerntrust/registration/SmsService.java
@@ -0,0 +1,11 @@
+package com.northerntrust.registration;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SmsService {
+
+    public void sendOtp(String phoneNumber, String otp) {
+        // Logic to send OTP via SMS
+    }
+}
