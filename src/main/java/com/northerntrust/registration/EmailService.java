bfd746c80f0099bade5be23b0e2d31499ba37270
Ramesh Appat <rameshappat@gmail.com>
2025-12-25 17:23:41 -0700

    feat: add EmailService.java

---
+++ src/main/java/com/northerntrust/registration/EmailService.java
@@ -0,0 +1,11 @@
+package com.northerntrust.registration;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    public void sendOtp(String email, String otp) {
+        // Logic to send OTP via email
+    }
+}
